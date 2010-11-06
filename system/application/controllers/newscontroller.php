bb680aa4f7b1624de10f659c9cc57b9b252d9ba9
zach.hinchy <zach@hinchy.us>
2010-11-06 14:25:07 -0500

    first revision of the news controller

---
+++ system/application/controllers/newscontroller.php
@@ -0,0 +1,31 @@
+<?php
+
+class Newscontroller extends Controller {
+	function Newscontroller() {
+		parent::Controller();
+		$this->load->database();
+		$this->load->library('ACMEData');
+		$this->load->model('usermodel');
+		$this->load->model('systemmodel');
+		$this->load->model('contentmodel');
+	}
+	
+	function view() {
+		$specificpage = $this->uri->segment(2);
+		if (!isset($specificpage)) $specificpage = 0;
+		
+		$config = $this->systemmodel->fetchConfig();
+		$userFields = $this->usermodel->fetchUserFields();
+		$news = $this->contentmodel->fetchNews($userFields);
+		$links = $this->systemmodel->fetchLinks();
+		
+		$data = Array(
+					'news' => $news,
+					'links' => $links,
+					'item' => $specificpage
+				);
+		
+		if ($specificpage > 0) $this->load->view($config['templategroup'].'_singlenews', $data);
+		else $this->load->view($config['templategroup'].'_news', $data);
+	}
+}
