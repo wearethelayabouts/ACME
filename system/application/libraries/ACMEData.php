c192484a4603392cf6c320c308f77a50f110470f
zach.hinchy <zach@hinchy.us>
2010-11-06 14:24:34 -0500

    added function for by-date sorting

--- system/application/libraries/ACMEData.php
+++ system/application/libraries/ACMEData.php
@@ -37,3 +37,9 @@ class ACMEData {
     	return $xml->asXML();
     }
 }
+function cmp_content($a, $b) {
+	if ($a['date'] == $b['date']) {
+		return 0;
+	}
+	return ($a['date'] > $b['date']) ? -1 : 1;
+}
