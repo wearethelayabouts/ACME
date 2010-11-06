c15b47435e1227dd9e328f615447b9dbb66ee09a
zach.hinchy <zach@hinchy.us>
2010-11-06 14:25:32 -0500

    added support for navigation with fetchContentNear, download ZIP and play all support as well

--- system/application/controllers/contentcontroller.php
+++ system/application/controllers/contentcontroller.php
@@ -15,14 +15,50 @@ class Contentcontroller extends Controller {
 			show_404('');
 		}
 		
+		$near = $this->contentmodel->fetchContentNear($content);
+		
 		$config = $this->systemmodel->fetchConfig();
-		$userFields = $this->usermodel->fetchUserFields();
 		$links = $this->systemmodel->fetchLinks();
 		
 		$data = Array(
 					'links' => $links,
 					'content' => $content,
+					'near' => $near
 				);
 		$this->load->view($config['templategroup'].'_contentpage', $data);
+	}
+	function downloadzip() {
+		$category = $this->uri->segment(2);
+		$config = $this->systemmodel->fetchConfig();
+		$links = $this->systemmodel->fetchLinks();
+		
+		$data = Array(
+					'links' => $links,
+					'category' => $category
+				);
+		$this->load->view($config['templategroup'].'_downloadzip', $data);
+	}
+	function playall() {
+		$category = $this->categorymodel->fetchCategorySlug($this->uri->segment(2));
+		
+		if (!$category) {
+			show_404('');
+		}
+		
+		$content = $this->contentmodel->fetchContentByCategory($category['id']);
+		
+		$config = $this->systemmodel->fetchConfig();
+		$links = $this->systemmodel->fetchLinks();
+		
+		if ($category['oldestFirst']) {
+			$content = array_reverse($content);
+		}
+		
+		$data = Array(
+					'links' => $links,
+					'category' => $category,
+					'content' => $content,
+				);
+		$this->load->view($config['templategroup'].'_playallpage', $data);
 	}
 }
