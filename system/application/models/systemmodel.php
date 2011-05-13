55cf34194803c740bba47464c1b19370142a2e9f
Zach Hinchy <zach@hinchy.us>
2011-05-13 12:13:11 -0500

    added a method that is a generic pagination html generator

--- system/application/models/systemmodel.php
+++ system/application/models/systemmodel.php
@@ -79,4 +79,99 @@ class Systemmodel extends Model {
 		
 		return $query;
 	}
+	
+	function paginate($page,$pagesamount,$urlprepend,$urlappend="") {
+		$paginationhtml = "";
+		if ($pagesamount > 1) {
+			$paginationhtml .= "<div class=\"page-select\">";
+			if ($page > 1) { 
+				$paginationhtml .= "<a href=\"";
+				$paginationhtml .= $urlprepend;
+				$paginationhtml .= "1";
+				$paginationhtml .= $urlappend;
+				$paginationhtml .= "\">&laquo;First</a> <a href=\"";
+				$paginationhtml .= $urlprepend;
+				$paginationhtml .= $page-1;
+				$paginationhtml .= $urlappend;
+				$paginationhtml .= "\">&lt;Previous</a>";
+			} else {
+				$paginationhtml .= "&laquo;First &lt;Previous";
+			}
+			
+			$paginationhtml .= " ";
+			
+			if ($pagesamount <= 5) {
+				for ($i=1; $i<=$pagesamount; $i++) {
+					if ($i == $page) $paginationhtml .= "<span class=\"currentpage\">";
+					else {
+						$paginationhtml .= "<a href=\"";
+						$paginationhtml .= $urlprepend;
+						$paginationhtml .= $i;
+						$paginationhtml .= $urlappend;
+						$paginationhtml .= "\">";
+					}
+					$paginationhtml .= $i;
+					if ($i == $page) $paginationhtml .= "</span>";
+					else $paginationhtml .= "</a>";
+					if ($i < $pagesamount) $paginationhtml .= " ";
+				}
+			} else {
+				
+				if (($page-2 >= 1) && ($page+2 <= $pagesamount)) {
+					$pagemin = $page-2;
+					$pagemax = $page+2;
+				} else if (($page-1 >= 1) && ($page+3 <= $pagesamount)) {
+					$pagemin = $page-1;
+					$pagemax = $page+3;
+				} else if (($page >= 1) && ($page+4 <= $pagesamount)) {
+					$pagemin = $page;
+					$pagemax = $page+4;
+				} else if (($page-3 >= 1) && ($page+1 <= $pagesamount)) {
+					$pagemin = $page-3;
+					$pagemax = $page+1;
+				} else if (($page-4 >= 1) && ($page <= $pagesamount)) {
+					$pagemin = $page-4;
+					$pagemax = $page;
+				}
+			
+				if ($pagemin > 1) $paginationhtml .= "&hellip; ";
+				
+				for ($i=$pagemin; $i<=$pagemax; $i++) {
+					if ($i == $page) $paginationhtml .= "<span class=\"currentpage\">";
+					else {
+						$paginationhtml .= "<a href=\"";
+						$paginationhtml .= $urlprepend;
+						$paginationhtml .= $i;
+						$paginationhtml .= $urlappend;
+						$paginationhtml .= "\">";
+					}
+					$paginationhtml .= $i;
+					if ($i == $page) $paginationhtml .= "</span> ";
+					else $paginationhtml .= "</a> ";
+				}
+			
+				if ($pagemax < $pagesamount) $paginationhtml .= "&hellip;";
+			}
+
+			$paginationhtml .= " ";
+			
+			if ($page < $pagesamount) { 
+				$paginationhtml .= "<a href=\"";
+				$paginationhtml .= $urlprepend;
+				$paginationhtml .= $page+1;
+				$paginationhtml .= $urlappend;
+				$paginationhtml .= "\">Next&gt;</a> <a href=\"";
+				$paginationhtml .= $urlprepend;
+				$paginationhtml .= $pagesamount;
+				$paginationhtml .= $urlappend;
+				$paginationhtml .= "\">Last&raquo;</a>";
+			} else {
+				$paginationhtml .= "Next&gt; Last&raquo;";
+			}
+			
+			$paginationhtml .= "</div>";
+			
+		}
+		return $paginationhtml;
+	}
 }
