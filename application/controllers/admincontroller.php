wearethelayabouts/ACME

commit 67d346fb274ef5f1ebc874876bd731beb0be57b8

Undoing breaking change in how user fields are returned in fetchUser().
Going back to returning them directly in the returned array instead of under fields as it broke a few templates.

application/models/usermodel.php
@@ -71,29 +71,27 @@ class Usermodel extends CI_Model {
 		foreach ($fieldMap as $field) {
 		  if (isset($fields[$field['slug']])) {
 			  if ($fieldData == 1) {
-				   $data['fields'][$field['slug']] = Array(
+				   $data[$field['slug']] = Array(
 								  'data' => $fields[$field['slug']]['data'],
 								  'metadata' => $field
 							  );
 			  } else {
-				  $data['fields'][$field['slug']] = $fields[$field['slug']];
+				  $data[$field['slug']] = $fields[$field['slug']];
 			  }
 		  } else {
 			  if ($fieldData == 1) {
-				   $data['fields'][$field['slug']] = Array(
+				   $data[$field['slug']] = Array(
 								  'data' => "",
 								  'metadata' => $field
 							  );
 			  } else {
-				  $data['fields'][$field['slug']] = "";
+				  $data[$field['slug']] = "";
 			  }
 		  }
 		}
 		} else {
-		$data['fields'] = $fields;
+			$data = $fields;
 		}
-		
-		$data['id'] = $uid;
 		
 		return $data;
 	}

commit b0d0a11a94c5d8a0d3adbde793da99417e1ea19f

Adds a "Published" checkbox to content editing and fixes issue in news editing with authors.
Closes #4 and fixes issue in news editing also caused by
67d346fb274ef5f1ebc874876bd731beb0be57b8

application/controllers/admincontroller.php
@@ -128,7 +128,8 @@ class Admincontroller extends CI_Controller {
 			
 			$postdata['name'] = $this->input->post('name');
 			$postdata['category_id'] = $this->input->post('category_id');
-			$postdata['body'] = $this->input->post('desc');
+			$postdata['published'] = $this->input->post('published');
+			$postdata['rating'] = $this->input->post('rating');
 			$postdata['rating'] = $this->input->post('rating');
 			$postdata['rating_description'] = $this->input->post('rating_description');
 			$postdata['custom_embed'] = $this->input->post('custom_embed');
@@ -244,6 +245,7 @@ class Admincontroller extends CI_Controller {
 								'name' => '',
 								'category_id' => '',
 								'slug' => '',
+								'published' => 0,
 								'date' => 0,
 								'body' => '',
 								'rating' => '',

application/models/usermodel.php
@@ -93,6 +93,8 @@ class Usermodel extends CI_Model {
 			$data = $fields;
 		}
 		
+		$data['uid'] = $uid;
+		
 		return $data;
 	}
 }

application/views/admin/content_edit.php
@@ -66,6 +66,18 @@
 					</tr>
 					<tr>
 						<td>
+							<p>Published (<span style="color: #f00;">*</span>)</p>
+							<p class="description">Should this be displayed to the users</p>
+						</td>
+						<td class="td">
+							<select name="published">
+							<option value="1"<?php if($content['published'] == 1) echo ' selected="yes"';?>>Yes</option>
+							<option value="0"<?php if($content['published'] == 0) echo ' selected="yes"';?>>No</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td>
 							<p>Date/Time</p>
 							<p class="description">Format: YYYY-MM-DD at hh:mm (24-hour time!)</p>
 							<p class="description">If left blank, will use current date/time. If set to a time later than now, content will remain unpublished until then.</p>
@@ -152,7 +164,7 @@
 							<div id="authorsbox">
 								<?php $i = 0; foreach ($content['authors'] as $author) { $i++; ?>
 								<input type="hidden" name="author_dbid_<?=$i?>" style="width: 80px;" value="<?=$author['db_id']?>" />
-								Author ID: <input type="text" name="author_id_<?=$i?>" style="width: 80px;" value="<?=$author['user']['id']?>" />
+								Author ID: <input type="text" name="author_id_<?=$i?>" style="width: 80px;" value="<?=$author['user']['uid']?>" />
 								&nbsp; &nbsp; &nbsp; <a href="javascript:void();" onclick="window.open('/toolbox/popup/users/select/<?=$i?>','new_win','width=350,height=650');">Browse Users...</a> &nbsp; &nbsp; &nbsp; Role: <input type="text" name="author_role_<?=$i?>" style="width: 160px;" value="<?=$author['role']?>" /><br />
 								<?php } ?>
 							</div><br />

application/views/admin/news_edit.php
@@ -73,7 +73,7 @@
 							<p class="description">User ID of the person that the post should be attributed to</p>
 						</td>
 						<td class="td">
-							<input type="text" name="author_id_1" id="author_id_1" style="width: 120px;" value="<?php echo $news['poster']['id']; ?>" /> &nbsp; &nbsp; &nbsp; <a href="javascript:void();" onclick="window.open('/toolbox/popup/users/select/1','new_win','width=350,height=650');">Browse Users...</a>
+							<input type="text" name="author_id_1" id="author_id_1" style="width: 120px;" value="<?php echo $news['poster']['uid']; ?>" /> &nbsp; &nbsp; &nbsp; <a href="javascript:void();" onclick="window.open('/toolbox/popup/users/select/1','new_win','width=350,height=650');">Browse Users...</a>
 						</td>
 					</tr>
 				</table>