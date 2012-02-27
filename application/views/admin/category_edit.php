wearethelayabouts/ACME

commit 6c12ab742c35e6b3b0bad4bf14cc4018d1eca0be

Admin Panel UI redesign

application/views/admin/page_edit.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
 		<title><?php echo $sitename; ?> &bull; Edit Page (Admin)</title>
-		<link href="<?php echo $baseurl; ?>includes/acme/admin.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo $baseurl; ?>includes/acme/rocket.css" rel="stylesheet" type="text/css" />
 		<script type="text/javascript" src="/includes/ext/tiny_mce/tiny_mce.js"></script>
 		<script type="text/javascript" >
 		tinyMCE.init({
@@ -12,10 +12,10 @@
 		        skin_variant : "silver",
 		        theme_advanced_toolbar_location : "top",
 		        theme_advanced_toolbar_align : "left",
-		        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,hr,removeformat,visualaid,|,sub,sup,|,charmap,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
-		        theme_advanced_buttons2 : "",
+		        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,hr,removeformat,visualaid,|,sub,sup",
+		        theme_advanced_buttons2 : "charmap,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
 		        theme_advanced_buttons3 : "",
-		        elements : "content",
+		        elements : "content, shortcontent",
 		        theme_advanced_statusbar_location : "bottom",
 		        theme_advanced_resizing : true,
 		        valid_elements : "*[*]"
@@ -23,54 +23,72 @@
 		</script>
 	</head>
 	<body>
-		<img src="<?php echo $baseurl; ?>includes/acme/logo.png" alt="ACME: Awesome Creative Media Engine" title="ACME: Awesome Creative Media Engine" />
-		<h1><?php echo $sitename; ?> Admin Toolbox</h1>
-		<h2><?php if ($editexisting) echo "Edit"; else echo "Add New"; ?> Page</h1>
-		<div class="mainbox" style="text-align: center;">
-			<?php foreach ($errors as $error) { ?>
-			<p class="message-error"><strong>ERROR:</strong> <?php echo $error?></p>
-			<?php } ?>
-			<p>
-				<?php if ($editexisting) { ?><a href="<?php echo $baseurl; ?>toolbox/delete/pages/<?php echo $page['id']; ?>/confirm">- Delete...</a>
-				<br /><br /><?php } ?>
-				Fields marked with a (<span style="color: #f00;">*</span>) are <em>required</em>.
-			</p>
-			<form action="<?php echo $thispageurl; ?>" method="post">
-				<input type="hidden" name="<?php echo $this->security->get_csrf_token_name()?>" value="<?php echo $this->security->get_csrf_hash()?>" />
-				<input type="hidden" name="commit" value="true" />
-				<input type="hidden" name="id" value="<?php echo $page['id']; ?>" />
-				<table class="maintable" style="text-align: left;">
-					<tr>
-						<td>
-							<p>Slug (<span style="color: #f00;">*</span>)</p>
-							<p class="description">Short content ID that goes in the URL (examples: <em>www.site.com/page/<strong>achkei</strong></em><br /><em>www.othersite.com/page/<strong>23</strong></em>)</p>
-						</td>
-						<td>
-							<input type="text" name="slug" style="width: 120px;" value="<?php echo $page['slug']; ?>" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<p>Custom CSS</p>
-							<p class="description">Shorter version of the article, displayed on the front page.</p>
-						</td>
-						<td>
-							<textarea name="custom_css" rows="8" cols="60"><?php echo $page['custom_css']; ?></textarea>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<p>Content (<span style="color: #f00;">*</span>)</p>
-							<p class="description">Page itself.</p>
-						</td>
-						<td>
-							<textarea name="content" rows="20" cols="60"><?php echo $page['content']; ?></textarea>
-						</td>
-					</tr>
-				</table>
-				<input type="submit" />
-			</form>
+		<div id="UISidebar">
+			<?php $this->load->view('admin/sidebar', Array('current' => 'pages'));?>
 		</div>
-		<p class="footer">Powered by <a href="http://acme.wearethelayabouts.com/">ACME</a> alpha 2 "Bucket" prerelease.</p>
+		<div id="UIContent">
+			<div class="UIContentSection">
+				<fieldset>
+					<legend>
+						<table class="UILegend">
+							<tr>
+								<td class="UILegendTitle"><?php if ($editexisting) echo "Edit"; else echo "Add"; ?> Page</td>
+								<td class="UILegendActions"><?php if ($editexisting) { ?><a href="<?php echo $baseurl; ?>toolbox/delete/pages/<?php echo $page['id']; ?>/confirm">Delete</a></td>
+							</tr>
+						</table>
+					</legend>
+					<div class="content nopadding">
+						<?php foreach ($errors as $error) { ?>
+						<p class="message-error"><strong>ERROR:</strong> <?php echo $error?></p><br /><br />
+						<?php } ?>
+						<div class="padding">
+							<?php } ?>
+							Fields marked with a (<span style="color: #f00;">*</span>) are <em>required</em>.
+						</div>
+						<form action="<?php echo $thispageurl; ?>" method="post">
+							<input type="hidden" name="<?php echo $this->security->get_csrf_token_name()?>" value="<?php echo $this->security->get_csrf_hash()?>" />
+							<input type="hidden" name="commit" value="true" />
+							<input type="hidden" name="id" value="<?php echo $page['id']; ?>" />
+							<table class="UITable" style="text-align: left;">
+								<tr>
+									<td width="350px">
+										<p>Slug (<span style="color: #f00;">*</span>)</p>
+										<p class="description">Short content ID that goes in the URL (examples: <em>www.site.com/page/<strong>achkei</strong></em><br /><em>www.othersite.com/page/<strong>23</strong></em>)</p>
+									</td>
+									<td>
+										<input type="text" name="slug" style="width: 120px;" value="<?php echo $page['slug']; ?>" />
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<p>Custom CSS</p>
+										<p class="description">CSS code used to additionally style the page's content.</p>
+									</td>
+									<td>
+										<textarea name="custom_css" rows="8" cols="60"><?php echo $page['custom_css']; ?></textarea>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<p>Content (<span style="color: #f00;">*</span>)</p>
+										<p class="description">Page itself.</p>
+									</td>
+									<td>
+										<textarea name="content" rows="20" cols="60"><?php echo $page['content']; ?></textarea>
+									</td>
+								</tr>
+							</table>
+							<input type="submit" />
+						</form>
+					</div>
+				</fieldset>
+			</div>
+		</div>
+		<div id="UIFooter">
+			&nbsp;
+			<div id="alignright">
+				<a href="/auth/logout">Logout</a>
+			</div>
+		</div>
 	</body>
 </html>

application/controllers/admincontroller.php
@@ -17,17 +17,7 @@ class Admincontroller extends CI_Controller {
 			redirect('/auth/login/');
 		}
 		
-		$config = $this->systemmodel->fetchConfig();
-		
-		$sitename = $this->config->item('site_name');
-		$baseurl = $this->config->item('base_url');
-		
-		$data = Array(
-			'sitename' => $sitename,
-			'baseurl' => $baseurl
-		);
-		
-		$this->load->view('admin/home', $data);
+		redirect("/toolbox/categories/");		
 	}
 	
 	// CONTENT //

application/views/admin/archives_wizard.php
@@ -2,50 +2,64 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
 		<title><?php echo $sitename; ?> &bull; Create an Archive (Admin)</title>
-		<link href="<?php echo $baseurl; ?>includes/acme/admin.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo $baseurl; ?>includes/acme/rocket.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<img src="<?php echo $baseurl; ?>includes/acme/logo.png" alt="ACME: Awesome Creative Media Engine" title="ACME: Awesome Creative Media Engine" />
-		<h1><?php echo $sitename; ?> Admin Toolbox</h1>
-		<h2>Create an Archive</h1>
-		<div class="mainbox" style="text-align: center;">
-			<?php foreach ($errors as $error) { ?>
-			<p class="message-error"><strong>ERROR:</strong> <?php echo $error?></p>
-			<?php } ?>
-			<p>
-				Fields marked with a (<span style="color: #f00;">*</span>) are <em>required</em>.
-			</p>
-			<form action="<?php echo $baseurl; ?>toolbox/archives/run/<?php echo $category['id']; ?>" method="post" name="form" id="form">
-				<input type="hidden" name="<?php echo $this->security->get_csrf_token_name()?>" value="<?php echo $this->security->get_csrf_hash()?>" />
-				<input type="hidden" name="act" value="create" />
-				<table class="maintable" style="text-align: left;">
-					<tr>
-						<td style="width: 150px;">
-							<p>Name</p>
-							<p class="description">Name of the category the archive will be generated for.</p>
-						</td>
-						<td>
-							<?php echo $category['name']; ?>
-						</td>
-					</tr>
-					<tr>
-						<td style="width: 150px;">
-							<p>Archive Type (<span style="color: #f00;">*</span>)</p>
-							<p class="description">How should we compress this?</p>
-						</td>
-						<td>
-							<select name="type">
-								<?php foreach ($formats as $ext => $name) { ?>
-								<option value="<?php echo $ext; ?>"><?php echo $name; ?></option>
-								<?php } ?>
-							</select>
-						</td>
-					</tr>
-				</table>
-				<input type="submit" />
-				(be patient after clicking this, it might take a while!)
-			</form>
+		<div id="UISidebar">
+			<?php $this->load->view('admin/sidebar', Array('current' => 'categories'));?>
 		</div>
-		<p class="footer">Powered by <a href="http://acme.wearethelayabouts.com/">ACME</a> alpha 2 "Bucket" prerelease.</p>
+		<div id="UIContent">
+			<div class="UIContentSection">
+				<fieldset>
+					<legend>Generate Archive</legend>
+					<div class="content nopadding">	
+						<?php foreach ($errors as $error) { ?>
+						<p class="message-error"><strong>ERROR:</strong> <?php echo $error?></p>
+						<?php } ?>
+						<div class="padding">
+							Fields marked with a (<span style="color: #f00;">*</span>) are <em>required</em>.
+						</div>
+						<form action="<?php echo $baseurl; ?>toolbox/archives/run/<?php echo $category['id']; ?>" method="post" name="form" id="form">
+							<input type="hidden" name="<?php echo $this->security->get_csrf_token_name()?>" value="<?php echo $this->security->get_csrf_hash()?>" />
+							<input type="hidden" name="act" value="create" />
+							<table class="UITable">
+								<tr>
+									<td style="width: 150px;">
+										<p>Name</p>
+										<p class="description">Name of the category the archive will be generated for.</p>
+									</td>
+									<td>
+										<?php echo $category['name']; ?>
+									</td>
+								</tr>
+								<tr>
+									<td style="width: 150px;">
+										<p>Archive Type (<span style="color: #f00;">*</span>)</p>
+										<p class="description">How should we compress this?</p>
+									</td>
+									<td>
+										<select name="type">
+											<?php foreach ($formats as $ext => $name) { ?>
+											<option value="<?php echo $ext; ?>"><?php echo $name; ?></option>
+											<?php } ?>
+										</select>
+									</td>
+								</tr>
+							</table>
+							<center>
+								<input type="submit" /><br />
+								(be patient after clicking this, it might take a while!)
+							</center>
+						</form>
+					</div>
+				</fieldset>
+			</div>
+		</div>
+		<div id="UIFooter">
+			&nbsp;
+			<div id="alignright">
+				<a href="/auth/logout">Logout</a>
+			</div>
+		</div>
 	</body>
 </html>

application/views/admin/category_view.php
@@ -2,36 +2,52 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
 		<title><?php echo $sitename; ?> &bull; View All Categories (Admin)</title>
-		<link href="<?php echo $baseurl; ?>includes/acme/admin.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo $baseurl; ?>includes/acme/rocket.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<img src="<?php echo $baseurl; ?>includes/acme/logo.png" alt="ACME: Awesome Creative Media Engine" title="ACME: Awesome Creative Media Engine" />
-		<h1><?php echo $sitename; ?> Admin Toolbox</h1>
-		<h2>View Categories</h1>
-		<div class="mainbox">
-			<p style="text-align: center;"><a href="<?php echo $baseurl; ?>toolbox/categories/add">+ Add New Category...</a></p>
-			<table class="maintable">
-				<tr>
-					<td>ID</td>
-					<td>Name</td>
-					<td>Parent Category</td>
-					<td>Published?</td>
-					<td>Date</td>
-				</tr>
-				<?php foreach ($categories as $piece) { ?>
-				<tr>
-					<td><strong><?php echo $piece['id']; ?></strong></td>
-					<td><a href="<?php echo $baseurl; ?>toolbox/categories/edit/<?php echo $piece['id']; ?>"><?php if (strlen($piece['name']) > 0) echo $piece['name']; else echo "(no description -- edit this category)"; ?></a></td>
-					<td class="smallish"><?php if ($piece['parent_id'] != 0) echo $categories[$piece['parent_id']]['name'];
-					else echo "None"; ?> </td>
-					<td><?php if ($piece['published'] != 0) echo "<span class=\"yes\">Yes</span>";
-					else echo "<span class=\"no\">No</span>";?></td>
-					<td><?php if ($piece['date'] > 0) { echo date('D, M j, Y @ g:i A',$piece['date']); } ?></td>
-				</tr>
-				<?php } ?>
-			</table>
-			<?php echo $paginationhtml; ?>
+		<div id="UISidebar">
+			<?php $this->load->view('admin/sidebar', Array('current' => 'categories'));?>
 		</div>
-		<p class="footer">Powered by <a href="http://acme.wearethelayabouts.com/">ACME</a> alpha 2 "Bucket" prerelease.</p>
+		<div id="UIContent">
+			<div class="UIContentSection">
+				<fieldset>
+					<table class="UILegend">
+						<tr>
+							<td class="UILegendTitle">Category</td>
+							<td class="UILegendActions"><a href="<?php echo $baseurl; ?>toolbox/categories/add">Add</a></td>
+						</tr>
+					</table>
+					<div class="content nopadding">	
+						<table class="UITable">
+							<tr>
+								<td>ID</td>
+								<td>Name</td>
+								<td>Parent Category</td>
+								<td>Published?</td>
+								<td>Date</td>
+							</tr>
+							<?php foreach ($categories as $piece) { ?>
+							<tr>
+								<td><strong><?php echo $piece['id']; ?></strong></td>
+								<td><a href="<?php echo $baseurl; ?>toolbox/categories/edit/<?php echo $piece['id']; ?>"><?php if (strlen($piece['name']) > 0) echo $piece['name']; else echo "(no description -- edit this category)"; ?></a></td>
+								<td class="smallish"><?php if ($piece['parent_id'] != 0) echo $categories[$piece['parent_id']]['name'];
+								else echo "None"; ?> </td>
+								<td><?php if ($piece['published'] != 0) echo "<span class=\"yes\">Yes</span>";
+								else echo "<span class=\"no\">No</span>";?></td>
+								<td><?php if ($piece['date'] > 0) { echo date('D, M j, Y @ g:i A',$piece['date']); } ?></td>
+							</tr>
+							<?php } ?>
+						</table>
+						<?php echo $paginationhtml; ?>
+					</div>
+				</div>
+			</fieldset>
+		</div>
+		<div id="UIFooter">
+			&nbsp;
+			<div id="alignright">
+				<a href="/auth/logout">Logout</a>
+			</div>
+		</div>
 	</body>
 </html>

application/views/admin/content_view.php
@@ -9,39 +9,55 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
 		<title><?php echo $sitename; ?> &bull; View All Content (Admin)</title>
-		<link href="<?php echo $baseurl; ?>includes/acme/admin.css" rel="stylesheet" type="text/css" />
+		<link href="<?php echo $baseurl; ?>includes/acme/rocket.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<img src="<?php echo $baseurl; ?>includes/acme/logo.png" alt="ACME: Awesome Creative Media Engine" title="ACME: Awesome Creative Media Engine" />
-		<h1><?php echo $sitename; ?> Admin Toolbox</h1>
-		<h2>View Content</h1>
-		<div class="mainbox">
-			<p style="text-align: center;"><a href="<?php echo $baseurl; ?>toolbox/content/add">+ Add New Content...</a></p>
-			<table class="maintable">
-				<tr>
-					<td><a href="<?php echo $baseurl; ?>toolbox/content/1/id/<?php if ($changetoasc == 'id') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'id') echo "<strong>"; ?>ID<?php if ($sortby == 'id') if ($sortdesc) echo "&#9660;</a></strong>"; else echo "&#9650;</a></strong>"; ?></td>
-					<td><a href="<?php echo $baseurl; ?>toolbox/content/1/name/<?php if ($changetoasc == 'name') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'name') echo "<strong>"; ?>Name<?php if ($sortby == 'name') if ($sortdesc) echo "&#9660;</a></strong>"; else echo "&#9650;</a></strong>"; ?></td>
-					<td><a href="<?php echo $baseurl; ?>toolbox/content/1/cat/<?php if ($changetoasc == 'cat') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'cat') echo "<strong>"; ?>Hub<?php if ($sortby == 'cat') { if ($sortdesc) echo "</strong> <em>(Subcategory)</em> &#9660;"; else echo "</strong> <em>(Subcategory)</em> &#9650;"; } else echo " <em>(Subcategory)</em>"; ?></a></td>
-					<td><a href="<?php echo $baseurl; ?>toolbox/content/1/date/<?php if ($changetoasc == 'date') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'date') echo "<strong>"; ?>Date<?php if ($sortby == 'date') if ($sortdesc) echo "&#9660;</a></strong>"; else echo "&#9650;</a></strong>"; ?></td>
-					<td>User Rating</td>
-					<td>Published?</td>
-				</tr>
-				<?php foreach ($content as $piece) {
-				if ($piece['date'] < 1) break; ?>
-				<tr>
-					<td><strong><?php echo $piece['id']; ?></strong></td>
-					<td><a href="<?php echo $baseurl; ?>toolbox/content/edit/<?php echo $piece['id']; ?>"><?php if (strlen($piece['name']) > 0) echo $piece['name']; else echo "(no description -- edit this piece of content)"; ?></a></td>
-					<td class="smallish"><?php echo $piece['hub']['name']; ?><?php if ($categorynames[$piece['id']] != $piece['hub']['name']) echo " <em class=\"smallish\">(", $categorynames[$piece['id']], ")</em>"; ?> </td>
-					<td class="tdalt smallish"><?php echo date('D, M j, Y @ g:i A',$piece['date']); ?></td>
-					<td>&nbsp;</td>
-					<td><?php if ($piece['date'] >= time()) echo "<span class=\"notyet\">Not Yet!</span>";
-					else if ($piece['published'] != 0) echo "<span class=\"yes\">Yes</span>";
-					else echo "<span class=\"no\">No</span>";?></td>
-				</tr>
-				<?php } ?>
-			</table>
-			<?php echo $paginationhtml; ?>
+		<div id="UISidebar">
+			<?php $this->load->view('admin/sidebar', Array('current' => 'content'));?>
 		</div>
-		<p class="footer">Powered by <a href="http://acme.wearethelayabouts.com/">ACME</a> alpha 2 "Bucket" prerelease.</p>
+		<div id="UIContent">
+			<div class="UIContentSection">
+				<fieldset>
+					<table class="UILegend">
+						<tr>
+							<td class="UILegendTitle">Content</td>
+							<td class="UILegendActions"><a href="<?php echo $baseurl; ?>toolbox/content/add">Add</a></td>
+						</tr>
+					</table>
+					<div class="content nopadding">	
+						<table class="UITable">
+							<tr>
+								<td><a href="<?php echo $baseurl; ?>toolbox/content/1/id/<?php if ($changetoasc == 'id') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'id') echo "<strong>"; ?>ID<?php if ($sortby == 'id') if ($sortdesc) echo "&#9660;</a></strong>"; else echo "&#9650;</a></strong>"; ?></td>
+								<td><a href="<?php echo $baseurl; ?>toolbox/content/1/name/<?php if ($changetoasc == 'name') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'name') echo "<strong>"; ?>Name<?php if ($sortby == 'name') if ($sortdesc) echo "&#9660;</a></strong>"; else echo "&#9650;</a></strong>"; ?></td>
+								<td><a href="<?php echo $baseurl; ?>toolbox/content/1/cat/<?php if ($changetoasc == 'cat') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'cat') echo "<strong>"; ?>Hub<?php if ($sortby == 'cat') { if ($sortdesc) echo "</strong> <em>(Subcategory)</em> &#9660;"; else echo "</strong> <em>(Subcategory)</em> &#9650;"; } else echo " <em>(Subcategory)</em>"; ?></a></td>
+								<td><a href="<?php echo $baseurl; ?>toolbox/content/1/date/<?php if ($changetoasc == 'date') echo 'a'; else echo 'd'; ?>"><?php if ($sortby == 'date') echo "<strong>"; ?>Date<?php if ($sortby == 'date') if ($sortdesc) echo "&#9660;</a></strong>"; else echo "&#9650;</a></strong>"; ?></td>
+								<td>User Rating</td>
+								<td>Published?</td>
+							</tr>
+							<?php foreach ($content as $piece) {
+							if ($piece['date'] < 1) break; ?>
+							<tr>
+								<td><strong><?php echo $piece['id']; ?></strong></td>
+								<td><a href="<?php echo $baseurl; ?>toolbox/content/edit/<?php echo $piece['id']; ?>"><?php if (strlen($piece['name']) > 0) echo $piece['name']; else echo "(no description -- edit this piece of content)"; ?></a></td>
+								<td class="smallish"><?php echo $piece['hub']['name']; ?><?php if ($categorynames[$piece['id']] != $piece['hub']['name']) echo " <em class=\"smallish\">(", $categorynames[$piece['id']], ")</em>"; ?> </td>
+								<td class="tdalt smallish"><?php echo date('D, M j, Y @ g:i A',$piece['date']); ?></td>
+								<td>&nbsp;</td>
+								<td><?php if ($piece['date'] >= time()) echo "<span class=\"notyet\">Not Yet!</span>";
+								else if ($piece['published'] != 0) echo "<span class=\"yes\">Yes</span>";
+								else echo "<span class=\"no\">No</span>";?></td>
+							</tr>
+							<?php } ?>
+						</table>
+						<center><?php echo $paginationhtml; ?></center>
+					</div>
+				</fieldset>
+			</div>
+		</div>
+		<div id="UIFooter">
+			&nbsp;
+			<div id="alignright">
+				<a href="/auth/logout">Logout</a>
+			</div>
+		</div>
 	</body>
 </html>

application/views/admin/delete_confirm.php
@@ -5,8 +5,6 @@
 		<link href="<?php echo $baseurl; ?>includes/acme/admin.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<img src="<?php echo $baseurl; ?>includes/acme/logo.png" alt="ACME: Awesome Creative Media Engine" title="ACME: Awesome Creative Media Engine" />
-		<h1><?php echo $sitename; ?> Admin Toolbox</h1>
 		<h2>Deleting <?php echo $what; ?> named <?php echo $name; ?></h1>
 		<div class="mainbox">
 			<form action="<?php echo $next; ?>" method="post">
@@ -16,6 +14,5 @@
 				<center><input type="submit" value="Delete" /></center>
 			</form>
 		</div>
-		<p class="footer">Powered by <a href="http://acme.wearethelayabouts.com/">ACME</a> alpha 2 "Bucket" prerelease.</p>
 	</body>
 </html>

commit 643038b75253b9806e2c9bd714605d94db94d824

Fixing issue where TinyMCE would break on a few pages
Introduced in 6c12ab742c35e6b3b0bad4bf14cc4018d1eca0be

application/views/admin/category_edit.php
@@ -15,7 +15,7 @@
 		        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,hr,removeformat,visualaid,|,sub,sup",
 		        theme_advanced_buttons2 : "charmap,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
 		        theme_advanced_buttons3 : "",
-		        elements : "content, shortcontent",
+		        elements : "body",
 		        theme_advanced_statusbar_location : "bottom",
 		        theme_advanced_resizing : true,
 		        valid_elements : "*[*]"

application/views/admin/content_edit.php
@@ -16,7 +16,7 @@
 		        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,hr,removeformat,visualaid,|,sub,sup",
 		        theme_advanced_buttons2 : "charmap,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
 		        theme_advanced_buttons3 : "",
-		        elements : "content, shortcontent",
+		        elements : "desc, custom_embed",
 		        theme_advanced_statusbar_location : "bottom",
 		        theme_advanced_resizing : true,
 		        valid_elements : "*[*]"

application/views/admin/page_edit.php
@@ -15,7 +15,7 @@
 		        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,hr,removeformat,visualaid,|,sub,sup",
 		        theme_advanced_buttons2 : "charmap,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
 		        theme_advanced_buttons3 : "",
-		        elements : "content, shortcontent",
+		        elements : "content",
 		        theme_advanced_statusbar_location : "bottom",
 		        theme_advanced_resizing : true,
 		        valid_elements : "*[*]"
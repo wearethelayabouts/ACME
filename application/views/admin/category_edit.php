643038b75253b9806e2c9bd714605d94db94d824
Henri Watson <henri@henriwatson.com>
2012-02-26 22:48:51 -0400

    Fixing issue where TinyMCE would break on a few pages
    Introduced in 6c12ab742c35e6b3b0bad4bf14cc4018d1eca0be

--- application/views/admin/category_edit.php
+++ application/views/admin/category_edit.php
@@ -15,7 +15,7 @@
 		        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,hr,removeformat,visualaid,|,sub,sup",
 		        theme_advanced_buttons2 : "charmap,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code",
 		        theme_advanced_buttons3 : "",
-		        elements : "content, shortcontent",
+		        elements : "body",
 		        theme_advanced_statusbar_location : "bottom",
 		        theme_advanced_resizing : true,
 		        valid_elements : "*[*]"
